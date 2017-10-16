fc1789ae34e5f1b0993eda3ea0764e27404fce84
JAVA <JAVA@192.168.0.77>
2017-10-16 11:10:44 +0200

    Task: #1: Persistencia + API: Leer Lista Clientes

--- public/app/app.config.js
+++ public/app/app.config.js
@@ -1,19 +1,24 @@
 'use strict';
 
 angular.module('petStore')
-    .config(function(
-        $locationProvider,
-        $routeProvider
-    ){
-        $locationProvider.html5Mode({ enabled: true });
-        $routeProvider
-            .when("/",{
-                template: "Acceder a los clientes: <a ng-href='sample'>Listado de clientes</a>"
-            })
-            .when("/sample",{
-                template: "<sample-module></sample-module>"
-            })
-            .otherwise({
-                template: "Other"
-            });
-    });
+.config(function(
+    $locationProvider,
+    $routeProvider
+){
+    $locationProvider.html5Mode({ enabled: true });
+    $routeProvider
+        .when("/",{
+            template: "Acceder a los clientes: <a ng-href='customers'>Listado de clientes</a>"
+        })
+        .when("/customers",{
+            template: "<sample-module></sample-module>"
+        })
+
+        .when("/customer",{
+            templateUrl: "./app/sample-module/customer.html"
+        })
+
+        .otherwise({
+            template: "<h1>Not Found</h1>"
+        });
+});
